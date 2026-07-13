f8731d061abaef0c7b97c91dd6597040dd441a6e
ccontrerasma <ccontrerasma@unsa.edu.pe>
2026-07-13 17:04:05 -0500

    Configuración inicial de Hilt

--- client-app/FoodApp/app/src/main/java/com/example/foodapp/di/AppModule.kt
+++ client-app/FoodApp/app/src/main/java/com/example/foodapp/di/AppModule.kt
@@ -1,4 +1,11 @@
 package com.example.foodapp.di
 
-class AppModule {
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
 }
